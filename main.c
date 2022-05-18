0ff8f6bba7dc751f1ac88ada525a07fb2c6eab3a
Michelle Kinda <resamichel@gmail.com>
2022-05-19 00:29:05 +0300

    New test

--- main.c
+++ main.c
@@ -1,152 +1,180 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
 #include "shell.h"
-#include "source.h"
-#include "parser.h"
-#include "executor.h"
-
 
 /**
- * main - loop function to get prompt
- * and read command
- * Return: Always 0.
+ * main - the main function
+ *
+ * Return: (Success) 0 always
+ * ------- (Fail) we drop out the looser :)
  */
-
-
-int main(int argc, char **argv)
+int main(void)
 {
-    char *cmd;
+	sh_t data;
+	int pl;
 
-    do
-    {
-        print_prompt1();
-
-        cmd = read_cmd();
-
-        if(!cmd)
-        {
-            exit(EXIT_SUCCESS);
-        }
-
-        if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
-        {
-            free(cmd);
-            continue;
-        }
-
-        if(strcmp(cmd, "exit\n") == 0)
-        {
-            free(cmd);
-            break;
-        }
-
-        struct source_s src;
-        src.buffer   = cmd;
-        src.bufsize  = strlen(cmd);
-        src.curpos   = INIT_SRC_POS;
-        parse_and_execute(&src);
-
-        free(cmd);
-
-    } while(1);
-
-    exit(EXIT_SUCCESS);
-}
-initsh();
-
-
-/*read command function */
-
-char *read_cmd(void)
-{
-    char buf[1024];
-    char *ptr = NULL;
-    char ptrlen = 0;
-
-    while(fgets(buf, 1024, stdin))
-    {
-        int buflen = strlen(buf);
-
-        if(!ptr)
-        {
-            ptr = malloc(buflen+1);
-        }
-        else
-        {
-            char *ptr2 = realloc(ptr, ptrlen+buflen+1);
-
-            if(ptr2)
-            {
-                ptr = ptr2;
-            }
-            else
-            {
-                free(ptr);
-                ptr = NULL;
-            }
-        }
-
-        if(!ptr)
-        {
-        struct source_s src;
-        src.buffer   = cmd;
-        src.bufsize  = strlen(cmd);
-        src.curpos   = INIT_SRC_POS;
-        parse_and_execute(&src);
-        return NULL;
-        }
-
-        strcpy(ptr+ptrlen, buf);
-
-        if(buf[buflen-1] == '\n')
-        {
-            if(buflen == 1 || buf[buflen-2] != '\\')
-            {
-                return ptr;
-            }
-
-            ptr[ptrlen+buflen-2] = '\0';
-            buflen -= 2;
-            print_prompt2();
-        }
-
-        ptrlen += buflen;
-    }
-
-    return ptr;
-  
+	_memset((void *)&data, 0, sizeof(data));
+	signal(SIGINT, signal_handler);
+	while (1)
+	{
+		index_cmd(&data);
+		if (read_line(&data) < 0)
+		{
+			if (isatty(STDIN_FILENO))
+				PRINT("\n");
+			break;
+		}
+		if (split_line(&data) < 0)
+		{
+			free_data(&data);
+			continue;
+		}
+		pl = parse_line(&data);
+		if (pl == 0)
+		{
+			free_data(&data);
+			continue;
+		}
+		if (pl < 0)
+		{
+			print_error(&data);
+			continue;
+		}
+		if (process_cmd(&data) < 0)
+		{
+			print_error(&data);
+			break;
+		}
+		free_data(&data);
+	}
+	free_data(&data);
+	exit(EXIT_SUCCESS);
 }
 
-/*function to parse and execute*/
-int parse_and_execute(struct source_s *src)
+/**
+ * read_line - read a line from the standard input
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int read_line(sh_t *data)
 {
-    skip_white_spaces(src);
+	char *csr_ptr, *end_ptr, c;
+	size_t size = BUFSIZE, read_st, length, new_size;
 
-    struct token_s *tok = tokenize(src);
-
-    if(tok == &eof_token)
-    {
-        return 0;
-    }
-
-    while(tok && tok != &eof_token)
-    {
-        struct node_s *cmd = parse_simple_command(tok);
-
-        if(!cmd)
-        {
-            break;
-        }
-
-        do_simple_command(cmd);
-        free_node_tree(cmd);
-        tok = tokenize(src);
-    }
-
-    return 1;
+	data->line = malloc(size * sizeof(char));
+	if (data->line == NULL)
+		return (FAIL);
+	if (isatty(STDIN_FILENO))
+		PRINT(PROMPT);
+	for (csr_ptr = data->line, end_ptr = data->line + size;;)
+	{
+		read_st = read(STDIN_FILENO, &c, 1);
+		if (read_st == 0)
+			return (FAIL);
+		*csr_ptr++ = c;
+		if (c == '\n')
+		{
+			*csr_ptr = '\0';
+			return (SUCCESS);
+		}
+		if (csr_ptr + 2 >= end_ptr)
+		{
+			new_size = size * 2;
+			length = csr_ptr - data->line;
+			data->line = _realloc(data->line, size * sizeof(char),
+						new_size * sizeof(char));
+			if (data->line == NULL)
+				return (FAIL);
+			size = new_size;
+			end_ptr = data->line + size;
+			csr_ptr = data->line + length;
+		}
+	}
 }
+#define DELIMITER " \n\t\r\a\v"
+/**
+ * split_line - splits line to tokens
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int split_line(sh_t *data)
+{
+	char *token;
+	size_t size = TOKENSIZE, new_size, i = 0;
 
+	if (_strcmp(data->line, "\n") == 0)
+		return (FAIL);
+	data->args = malloc(size * sizeof(char *));
+	if (data->args == NULL)
+		return (FAIL);
+	token = strtok(data->line, DELIMITER);
+	if (token == NULL)
+		return (FAIL);
+	while (token)
+	{
+		data->args[i++] =  token;
+		if (i + 2 >= size)
+		{
+			new_size = size * 2;
+			data->args = _realloc(data->args, size * sizeof(char *),
+					new_size * sizeof(char *));
+			if (data->args == NULL)
+				return (FAIL);
+			size = new_size;
+		}
+		token = strtok(NULL, DELIMITER);
+	}
+	data->args[i] = NULL;
+	return (0);
+}
+#undef DELIMITER
+#define DELIMITER ":"
+/**
+ * parse_line - parses arguments to valid command
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int parse_line(sh_t *data)
+{
+	if (is_path_form(data) > 0)
+		return (SUCCESS);
+	if (is_builtin(data) > 0)
+	{
+		if (handle_builtin(data) < 0)
+			return (FAIL);
+		return (NEUTRAL);
+	}
+	is_short_form(data);
+	return (SUCCESS);
+}
+#undef DELIMITER
+/**
+ * process_cmd - process command and execute process
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
+ */
+int process_cmd(sh_t *data)
+{
+	pid_t pid;
+	int status;
 
-
+	pid = fork();
+	if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);
+		if (execve(data->cmd, data->args, environ) < 0)
+		data->error_msg = _strdup("not found\n");
+			return (FAIL);
+	}
+	else
+	{
+		waitpid(pid, &status, WUNTRACED);
+	}
+	return (0);
+}
--- wordexp.c
+++
@@ -1,7 +0,0 @@
-char *wordlist_to_str(struct word_s *word);
-void delete_char_at(char *str, size_t index);
-int is_name(char *str);
-size_t find_closing_quote(char *data);
-size_t find_closing_brace(char *data);
-char *substitute_str(char *s1, char *s2, size_t start, size_t end);
-int substitute_word(char **pstart, char **p, size_t len, char *(func)(char *), int add_quotes);
